9e0001cfb87f563673db93e83411046504a5e533
ROHANBAIJU <147437424+ROHANBAIJU@users.noreply.github.com>
2025-11-20 08:25:00 +0530

    UPDATED FOR HOSTINGG

--- frontend/explainnet-ui/src/environments/environment.prod.ts
+++ frontend/explainnet-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Use Render backend URL in production
-  apiBaseUrl: 'https://explainnet-backend.onrender.com/api' // Update this after deploying to Render
+  apiBaseUrl: 'https://explainnet-backend.onrender.com' // Update this after deploying to Render --DONE
 };
